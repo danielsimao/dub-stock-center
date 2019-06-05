8d9cf83fc09afc5ff6e640a103fd7bd6055e6b9a
Daniel Simão <rui.daniel.simao@gmail.com>
2019-06-05 17:38:44 +0100

    serving stock in wanted curr

--- routes/api/stock.js
+++ routes/api/stock.js
@@ -7,18 +7,52 @@ const axios = require("axios");
 //@desc Get Stock by Date and symbol
 //@acess Public
 
-router.get("/", (req, res) => {
-  const { date, symbol } = req.body;
-  axios
-    .get(
+router.get("/", async (req, res) => {
+  // const { date, symbol, currency } = req.body;
+
+  const currency = "DKK";
+  let symbol = "AMZN";
+  let date = "2018-01-02";
+
+  const { usd, selectedCurr } = await getAllCurrenciesEUR(currency);
+
+  const stockUSD = await getStockValue(symbol, date);
+
+  const stock = stockConverter(stockUSD, usd, selectedCurr);
+
+  res.send({ symbol, currency, stock });
+});
+
+async function getStockValue(symbol, date) {
+  try {
+    const { data } = await axios.get(
       `https://api.worldtradingdata.com/api/v1/history_multi_single_day?symbol=${symbol}&date=${date}&api_token=${config.get(
         "worldTradingDataAPIKey"
       )}`
-    )
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(err => res.status(404).json({ success: false }));
-});
+    );
+
+    return data.data[symbol].close;
+  } catch (e) {
+    res.status(404).json({ success: false });
+  }
+}
+
+async function getAllCurrenciesEUR(currency) {
+  try {
+    const { data } = await axios.get(
+      `http://data.fixer.io/api/latest?access_key=${config.get("fixerAPIKey")}
+    `
+    );
+
+    return { usd: data.rates["USD"], selectedCurr: data.rates[currency] };
+  } catch (e) {
+    res.status(404).json({ success: false });
+  }
+}
+
+function stockConverter(stock, usd, currency) {
+  const eur = stock / usd;
+  return eur * currency;
+}
 
 module.exports = router;
