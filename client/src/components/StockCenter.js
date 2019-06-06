c22c96ef4a25f6d4578e355375d8737a88877e64
Daniel Simão <rui.daniel.simao@gmail.com>
2019-06-06 19:55:23 +0100

    More improvements

--- client/src/components/StockCenter.js
+++ client/src/components/StockCenter.js
@@ -12,7 +12,9 @@ import {
   Label,
   CustomInput,
   Row,
-  Col
+  Col,
+  ButtonGroup,
+  Alert
 } from "reactstrap";
 import { getCurrencies } from "../actions/currencyActions";
 import { connect } from "react-redux";
@@ -35,8 +37,10 @@ const stocks = [
 const StockCenter = props => {
   const [isOpen, toggle] = useState(false);
   const [currencies, setCurrencies] = useState(null);
-  const [currency, setCurrency] = useState(null);
+  const [currency, setCurrency] = useState("USD");
   const [date, setDate] = useState(new Date());
+  const [symbol, setSymbol] = useState("AMZN");
+  const [stock, setStock] = useState(1740.0);
 
   useEffect(() => setCurrencies(props.currency.currency.currencies), [
     props.currency.currency.currencies
@@ -49,11 +53,14 @@ const StockCenter = props => {
     if (!props.currency.currency.loading && !currencies) fetchItems();
   }, [currencies, props]);
 
+  const search = () => {};
+
   return (
     <div>
       <Container>
         <Form style={{ justifyContent: "center" }} inline>
           {" "}
+          {console.log(currency)}
           <FormGroup className="mb-2 mr-sm-5 mb-sm-0">
             <Label for="exampleCustomSelect" className="mr-sm-2">
               Currency:
@@ -63,6 +70,7 @@ const StockCenter = props => {
               id="exampleCustomSelect"
               name="customSelect"
               placeholder={currency}
+              ini
             >
               {currencies &&
                 currencies.map((currency, id) => (
@@ -93,22 +101,51 @@ const StockCenter = props => {
               </InputGroupButtonDropdown>
             </InputGroup>
           </FormGroup>
-          <Button color="secondary">Search</Button>{" "}
+          <FormGroup className="mb-2 mr-sm-5 mb-sm-0">
+            <Label for="stock" className="mr-sm-2">
+              Stock Symbol:
+            </Label>
+
+            <Input
+              id="stock"
+              placeholder={symbol}
+              onChange={e => setSymbol(e)}
+            />
+          </FormGroup>
+          <Button>Search</Button>
         </Form>
 
-        <Row style={{ justifyContent: "center" }}>
-          {stocks.map((stock, id) => (
-            <Col key={id}>
-              <Button
-                style={{ width: "5rem", marginTop: "1rem" }}
-                outline
-                color="secondary"
-              >
-                {stock}
-              </Button>{" "}
-            </Col>
-          ))}
-        </Row>
+        <div
+          style={{
+            margin: "auto",
+            width: "200px",
+            marginTop: "10rem",
+            padding: 10
+          }}
+        >
+          <h6
+            style={{ fontWeight: 20, color: "grey" }}
+          >{`NASDAQ: ${symbol}`}</h6>
+          <h2 style={{ fontWeight: 20, marginBottom: 2 }}>
+            {new Intl.NumberFormat({
+              minimumFractionDigits: 2
+            }).format(stock)}
+            <span
+              style={{
+                fontSize: 20,
+                color: "grey",
+                fontWeight: 10,
+                marginLeft: 4
+              }}
+            >
+              USD
+            </span>
+          </h2>
+          <span style={{ marginTop: 0 }}>
+            {" "}
+            {format(date, "ddd D MMM, YYYY")}{" "}
+          </span>
+        </div>
       </Container>
     </div>
   );
