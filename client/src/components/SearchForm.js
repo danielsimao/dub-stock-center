3018963eec9b38c697d3ed77d73c0f9b86b7012f
Daniel Simão <rui.daniel.simao@gmail.com>
2019-06-08 18:31:50 +0100

    Refactor code - SearchForm

---
+++ client/src/components/SearchForm.js
@@ -0,0 +1,110 @@
+import React, { useState, useEffect } from "react";
+import {
+  InputGroup,
+  InputGroupButtonDropdown,
+  Input,
+  Button,
+  DropdownToggle,
+  DropdownMenu,
+  Form,
+  FormGroup,
+  Label,
+  CustomInput
+} from "reactstrap";
+import { getCurrencies } from "../actions/currencyActions";
+import { connect } from "react-redux";
+import Calendar from "react-calendar";
+import { format } from "date-fns";
+
+const StockCenter = (props, { setSearch }) => {
+  const [isOpen, toggle] = useState(false);
+  const [currencies, setCurrencies] = useState(null);
+  const [currency, setCurrency] = useState("AED");
+  const [date, setDate] = useState(new Date());
+  const [symbol, setSymbol] = useState("AMZN");
+
+  const searchHandler = e => {
+    e.preventDefault();
+    setSearch({ currency, date, symbol });
+  };
+
+  useEffect(() => setCurrencies(props.currency.currency.currencies), [
+    props.currency.currency.currencies
+  ]);
+
+  useEffect(() => {
+    async function fetchItems() {
+      await props.getCurrencies();
+    }
+    if (!props.currency.currency.loading && !currencies) fetchItems();
+  }, [currencies, props]);
+
+  return (
+    <Form onSubmit={searchHandler} style={{ justifyContent: "center" }} inline>
+      {" "}
+      <FormGroup className="mb-2 mr-sm-4 mb-sm-0 mt-4">
+        <Label for="exampleCustomSelect" className="mr-sm-2">
+          Currency:
+        </Label>
+        <CustomInput
+          type="select"
+          id="exampleCustomSelect"
+          name="customSelect"
+          onChange={e => setCurrency(e.target.value)}
+        >
+          {currencies &&
+            currencies.map((currency, id) => (
+              <option key={id}>{currency}</option>
+            ))}
+        </CustomInput>
+      </FormGroup>
+      <FormGroup className="mb-2 mr-sm-4 mb-sm-0 mt-4">
+        <Label for="date" className="mr-sm-2">
+          Date:
+        </Label>
+        <InputGroup id="date">
+          <Input placeholder={format(date, "YYYY-MM-DD")} />
+          <InputGroupButtonDropdown
+            addonType="prepend"
+            isOpen={isOpen}
+            toggle={() => toggle(!isOpen)}
+          >
+            <DropdownToggle split outline />
+            <DropdownMenu>
+              <Calendar
+                activeStartDate={date}
+                onChange={date => {
+                  setDate(date);
+                  toggle(!isOpen);
+                }}
+              />{" "}
+            </DropdownMenu>
+          </InputGroupButtonDropdown>
+        </InputGroup>
+      </FormGroup>
+      <FormGroup className="mb-2 mr-sm-4 mb-sm-0 mt-4">
+        <Label for="stock" className="mr-sm-2">
+          Stock Symbol:
+        </Label>
+
+        <Input
+          id="stock"
+          placeholder={symbol}
+          onChange={e => setSymbol(e.target.value)}
+        />
+      </FormGroup>
+      <Button type="submit" className="mt-4">
+        Search
+      </Button>
+    </Form>
+  );
+};
+
+const mapStateToProps = state => ({
+  currency: state
+});
+
+export default connect(
+  mapStateToProps,
+  { getCurrencies }
+)(StockCenter);
--- client/src/components/StockCenter.js
+++ client/src/components/StockCenter.js
@@ -1,31 +1,11 @@
-import React, { useState, useEffect } from "react";
-import {
-  InputGroup,
-  InputGroupButtonDropdown,
-  Input,
-  Button,
-  Container,
-  DropdownToggle,
-  DropdownMenu,
-  Form,
-  FormGroup,
-  Label,
-  CustomInput,
-  Alert
-} from "reactstrap";
-import { getCurrencies } from "../actions/currencyActions";
-import { connect } from "react-redux";
-import Calendar from "react-calendar";
+import React, { useState } from "react";
+import { Container, Alert } from "reactstrap";
 import { format } from "date-fns";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import SearchForm from "./SearchForm";
 
-const StockCenter = props => {
-  const [isOpen, toggle] = useState(false);
-  const [currencies, setCurrencies] = useState(null);
-  const [currency, setCurrency] = useState("AED");
-  const [date, setDate] = useState(new Date());
-  const [symbol, setSymbol] = useState("AMZN");
+const StockCenter = () => {
   const [search, setSearch] = useState({
     currency: "AED",
     date: new Date(),
@@ -42,22 +22,6 @@ const StockCenter = props => {
     }
   `;
 
-  const searchHandler = e => {
-    e.preventDefault();
-    setSearch({ ...search, currency, date, symbol });
-  };
-
-  useEffect(() => setCurrencies(props.currency.currency.currencies), [
-    props.currency.currency.currencies
-  ]);
-
-  useEffect(() => {
-    async function fetchItems() {
-      await props.getCurrencies();
-    }
-    if (!props.currency.currency.loading && !currencies) fetchItems();
-  }, [currencies, props]);
-
   return (
     <Query
       query={QUERY_STOCK}
@@ -71,67 +35,7 @@ const StockCenter = props => {
         return (
           <div>
             <Container>
-              <Form
-                onSubmit={searchHandler}
-                style={{ justifyContent: "center" }}
-                inline
-              >
-                {" "}
-                <FormGroup className="mb-2 mr-sm-4 mb-sm-0 mt-4">
-                  <Label for="exampleCustomSelect" className="mr-sm-2">
-                    Currency:
-                  </Label>
-                  <CustomInput
-                    type="select"
-                    id="exampleCustomSelect"
-                    name="customSelect"
-                    onChange={e => setCurrency(e.target.value)}
-                  >
-                    {currencies &&
-                      currencies.map((currency, id) => (
-                        <option key={id}>{currency}</option>
-                      ))}
-                  </CustomInput>
-                </FormGroup>
-                <FormGroup className="mb-2 mr-sm-4 mb-sm-0 mt-4">
-                  <Label for="date" className="mr-sm-2">
-                    Date:
-                  </Label>
-                  <InputGroup id="date">
-                    <Input placeholder={format(date, "YYYY-MM-DD")} />
-                    <InputGroupButtonDropdown
-                      addonType="prepend"
-                      isOpen={isOpen}
-                      toggle={() => toggle(!isOpen)}
-                    >
-                      <DropdownToggle split outline />
-                      <DropdownMenu>
-                        <Calendar
-                          activeStartDate={date}
-                          onChange={date => {
-                            setDate(date);
-                            toggle(!isOpen);
-                          }}
-                        />{" "}
-                      </DropdownMenu>
-                    </InputGroupButtonDropdown>
-                  </InputGroup>
-                </FormGroup>
-                <FormGroup className="mb-2 mr-sm-4 mb-sm-0 mt-4">
-                  <Label for="stock" className="mr-sm-2">
-                    Stock Symbol:
-                  </Label>
-
-                  <Input
-                    id="stock"
-                    placeholder={symbol}
-                    onChange={e => setSymbol(e.target.value)}
-                  />
-                </FormGroup>
-                <Button type="submit" className="mt-4">
-                  Search
-                </Button>
-              </Form>
+              <SearchForm setSearch={setSearch} />
 
               {error ? (
                 <Alert
@@ -156,13 +60,13 @@ const StockCenter = props => {
                     }}
                   >
                     {" "}
-                    <h6
-                      style={{ fontWeight: 20, color: "grey" }}
-                    >{`NASDAQ: ${symbol}`}</h6>
+                    <h6 style={{ fontWeight: 20, color: "grey" }}>{`NASDAQ: ${
+                      search.symbol
+                    }`}</h6>
                     <h2 style={{ fontWeight: 20, marginBottom: 2 }}>
                       {new Intl.NumberFormat({
                         minimumFractionDigits: 2
-                      }).format(data.getStock[symbol].close)}
+                      }).format(data.getStock[search.symbol].close)}
                       <span
                         style={{
                           fontSize: 20,
@@ -176,7 +80,7 @@ const StockCenter = props => {
                     </h2>
                     <span style={{ marginTop: 0 }}>
                       {" "}
-                      {format(date, "ddd D MMM, YYYY")}{" "}
+                      {format(search.date, "ddd D MMM, YYYY")}{" "}
                     </span>
                   </div>
                 )
@@ -189,11 +93,4 @@ const StockCenter = props => {
   );
 };
 
-const mapStateToProps = state => ({
-  currency: state
-});
-
-export default connect(
-  mapStateToProps,
-  { getCurrencies }
-)(StockCenter);
+export default StockCenter;
